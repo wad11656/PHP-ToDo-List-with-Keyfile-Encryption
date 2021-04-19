14c520b073f1fdece8250eec2428428a8adfcbee
wad11656 <wad11656@byu.edu>
2021-04-19 00:36:37 -0600

    Add index and style sheet files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List Application PHP and MySQL</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="heading">
+		<h2 style="font-style: 'Helvetica';">ToDo List Application PHP and MySQL database</h2>
+	</div>
+	<form method="post" action="index.php" class="input_form">
+		<input type="text" name="task" class="task_input">
+		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+	</form>
+</body>
+</html>
