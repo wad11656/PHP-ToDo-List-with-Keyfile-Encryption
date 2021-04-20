65a178d5cd717dc3428b8a83eaead0bb1aa292bb
wad11656 <wad11656@byu.edu>
2021-04-20 01:00:19 -0600

    Store DB credentials in encrypted .env stored outside of site's root directory

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<?php
+    // Create error variable
+	$errors = "";
+
+	// Define includes
+    require_once __DIR__.'\vendor\autoload.php';
+	require_once("defuse-crypto.phar");
+
+	// Load .env (contains DB creds)
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+    $dotenv->load();
+
+	// Load .env decryption key
+	$keyContents = file_get_contents('C:\keyfile');
+	$key = Defuse\Crypto\Key::loadFromAsciiSafeString($keyContents);
+
+	// Decrypt DB creds in .env
+	$server = Defuse\Crypto\Crypto::decrypt($_ENV['SERVER'], $key);
+	$user = Defuse\Crypto\Crypto::decrypt($_ENV['USER'], $key);
+	$pass = Defuse\Crypto\Crypto::decrypt($_ENV['PASS'], $key);
+	$database = Defuse\Crypto\Crypto::decrypt($_ENV['DATABASE'], $key);
+
+	// Create MySQL connection variable
+	$db = mysqli_connect($server, $user, $pass, $database);
+
+	// insert a quote if submit button is clicked
+	if (isset($_POST['submit'])) {
+		if (empty($_POST['task'])) {
+			$errors = "You must fill in the task";
+		}else{
+			$task = $_POST['task'];
+			$sql = "INSERT INTO todos_tbl (todo_task) VALUES ('$task')";
+			mysqli_query($db, $sql);
+			header('location: index.php');
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +47,9 @@
 		<h2 style="font-style: 'Helvetica';">ToDo List Application PHP and MySQL database</h2>
 	</div>
 	<form method="post" action="index.php" class="input_form">
+	<?php if (isset($errors)) { ?>
+		<p><?php echo $errors; ?></p>
+	<?php } ?>
 		<input type="text" name="task" class="task_input">
 		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
 	</form>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,78 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3ed013153053ded43d4a0c222cc36eb5412d18a0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3ed013153053ded43d4a0c222cc36eb5412d18a0',
+    ),
+    'defuse/php-encryption' => 
+    array (
+      'pretty_version' => 'v2.3.1',
+      'version' => '2.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '77880488b9954b7884c25555c2a0ea9e7053f9d2',
+    ),
+    'paragonie/random_compat' => 
+    array (
+      'pretty_version' => 'v2.0.20',
+      'version' => '2.0.20.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f1f60250fccffeaf5dda91eea1c018aed1adc2a',
+    ),
+    'phpoption/phpoption' => 
+    array (
+      'pretty_version' => '1.7.5',
+      'version' => '1.7.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '994ecccd8f3283ecf5ac33254543eb0ac946d525',
+    ),
+    'symfony/dotenv' => 
+    array (
+      'pretty_version' => 'v3.4.47',
+      'version' => '3.4.47.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1022723ac4f56b001d99691d96c6025dbf1404f1',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.19.0',
+      'version' => '1.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aed596913b70fae57be53d86faa2e9ef85a2297b',
+    ),
+    'vlucas/phpdotenv' => 
+    array (
+      'pretty_version' => 'v4.2.0',
+      'version' => '4.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'da64796370fc4eb03cc277088f6fede9fde88482',
+    ),
+  ),
+);
